295873a73a156399a9ac7917fca0907203fd4540
charu1998 <31506061+charu1998@users.noreply.github.com>
2018-01-20 14:26:45 +0530

    Create nk.c

---
+++ nk.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int n,k,i,j,sum=0;
+	scanf("%d %d",&n,&k);
+	for(i=1;i<=n;i++)
+	{
+		printf("%d\t",i);
+	}
+	for(j=1;j<=k;j++)
+	{
+		sum=sum+1;
+			}
+			printf("sum is %d",sum);
+}
