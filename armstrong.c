451309c01a9e8ed64a37ddcd089df13d86b72912
charu1998 <31506061+charu1998@users.noreply.github.com>
2018-01-21 10:29:20 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int n,d,res=0,t;
+	printf("enter n value");
+	scanf("%d",&n);
+	n=t;
+	while(n!=0)
+	{
+		d=n%10;
+		res=res+(d*d*d);
+		n=n/10;
+	}
+	if(t==res)
+	{
+		printf("armstrong");
+	}
+	else
+	{
+		printf("no");
+	}
+	}
