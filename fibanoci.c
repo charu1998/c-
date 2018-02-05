afc1d84d8e18e6a3f645abe6df6253776c2a51a1
charu1998 <31506061+charu1998@users.noreply.github.com>
2018-02-05 17:01:58 +0530

    Create fibanoci.c

---
+++ fibanoci.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int f1=0,f2=0,f,n,i;
+	printf("enter the values");
+	scanf("%d",&n);
+	for(i=2;i<=2;i++)
+	{
+		f=f1+f2;
+		printf("%d",f);
+		f1=f2;
+		f2=f;
+	}
+	return 0;
+ 
+}
