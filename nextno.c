6599b8c5f79969b16d802c4786cde58b3367c611
charu1998 <31506061+charu1998@users.noreply.github.com>
2018-02-09 10:17:09 +0000

    Create nextno.c

---
+++ nextno.c
@@ -0,0 +1,10 @@
+    #include<stdio.h>
+    int main()
+    {
+    	int m,n;
+    	printf("enter the number");
+    	scanf("%d",&m);
+    	n=m+1;
+    	printf("next number is %d",n);
+    	return 0;
+    }
