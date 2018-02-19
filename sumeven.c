2f4b960fd35db2525ab81c95ed65ed0772e5a086
charu1998 <31506061+charu1998@users.noreply.github.com>
2018-02-19 15:55:52 +0530

    Create sumeven.c

---
+++ sumeven.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int n,m,s;
+	printf("enter n,m values");
+	scanf("%d %d",&n,&m);
+	s=n+m;
+	if(s%2==0)
+	{
+		printf("even");
+	}
+	else
+	{
+		printf("odd");
+	}
+	return 0;
+	
+}
