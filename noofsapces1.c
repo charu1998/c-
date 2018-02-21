ac7bab34d6697073fa24622a10a6f0303fd97181
charu1998 <31506061+charu1998@users.noreply.github.com>
2018-02-21 16:25:52 +0530

    Create noofsapces1.c

---
+++ noofsapces1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	char a[100];
+	int count=0,i,len;
+		printf("enter the sentence");
+	gets(a);
+	len=strlen(a);
+	for(i=0;i<len;i++)
+	{
+		
+	if(a[i]==' ')
+	{
+		count ++;
+	}
+	}
+	printf("no of spaces are %d",count);
+}
