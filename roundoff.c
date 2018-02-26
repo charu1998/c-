e343fb0555880486c2bcd96d1f9a9f9176a9f0b0
charu1998 <31506061+charu1998@users.noreply.github.com>
2018-02-26 16:03:06 +0530

    Create roundoff.c

---
+++ roundoff.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    float f;
+    int m,n,l;
+    printf("enter the value");
+    scanf("%f",&f);
+    m=f*10;
+    n=m/10;
+    l=n+1;
+    printf("the round off value is %d",l);
+    return 0;
+}
