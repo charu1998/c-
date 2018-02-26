cac4f2052564801997d34937d6f456d1b87e0843
charu1998 <31506061+charu1998@users.noreply.github.com>
2018-02-26 16:32:23 +0530

    Create perfectsquare.c

---
+++ perfectsquare.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n1,n2,m;
+    printf("enter 2 values");
+    scanf("%d %d",&n1,&n2);
+    m=n1*n2;
+    if(n1=n2)
+    {
+        printf("yes");
+        }
+        else
+        {
+            printf("no");
+            }
+            return 0;
+}
