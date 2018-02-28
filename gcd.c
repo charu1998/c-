f9fcc95ae5be07a2ac69f119a6249a58769e6693
charu1998 <31506061+charu1998@users.noreply.github.com>
2018-02-28 16:38:56 +0530

    Create gcd.c

---
+++ gcd.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,gcd,t;
+    printf("enter 2 values");
+    scanf("%d %d",&a,&b);
+    c=a;
+    d=b;
+    while(d!=0)
+    {
+        t=d;
+        d=c%d;
+        c=t;
+    }
+    gcd=c;
+    printf("GCD value is %d",gcd);
+    return 0;
+}
