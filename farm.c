b712e704c1c56b53d72ce7c0b6e542bc3e8bd48b
charu1998 <31506061+charu1998@users.noreply.github.com>
2018-02-28 15:48:51 +0530

    Create farm.c

---
+++ farm.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float l,b,a;
+    printf("enter the length and breadth");
+    scanf("%f %f",&l,&b);
+    a=l*b;
+    printf("the area of the form is %f",a);
+    return 0;
+}
